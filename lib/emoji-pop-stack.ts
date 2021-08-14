f4743dea99c4d56f27d9c3ee3a0d1e56e578551d
wiwium <ketanchaudhari1998@gmail.com>
2021-08-14 22:46:51 +0530

    working in js

--- lib/emoji-pop-stack.ts
+++ lib/emoji-pop-stack.ts
@@ -3,19 +3,26 @@ import * as apigw2 from '@aws-cdk/aws-apigatewayv2';
 import * as apigw2i from '@aws-cdk/aws-apigatewayv2-integrations';
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as iam from "@aws-cdk/aws-iam";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
 
 export class EmojiPopStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const counterTable = new dynamodb.Table(this, "counterTable", {
+      partitionKey: { name: 'connections', type: dynamodb.AttributeType.STRING },
+    });
 
     const counterLambda = new lambda.Function(this, "counterLambda", {
       handler: "emoji-rain-handler.handler2",
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda'),
       environment: {
-        TEST: "test-env",
+        DYNAMODB_NAME: counterTable.tableName,
       }
     });
+
+    
 
     const webSocketApi = new apigw2.WebSocketApi(this, 'mywsapi', {
       connectRouteOptions: { integration: new apigw2i.LambdaWebSocketIntegration({ handler: counterLambda }) },
@@ -46,6 +53,7 @@ export class EmojiPopStack extends cdk.Stack {
     // there must be a better way to do this!!.
     counterLambda.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonAPIGatewayInvokeFullAccess"))
     // addPermission to give resouce base policy-> pincliple is who will have the access.
+    counterTable.grantFullAccess(counterLambda);
 
 
 
