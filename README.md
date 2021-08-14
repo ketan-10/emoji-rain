0946919993add93f4fc75cf6d4194c5c0bdcf768
wiwium <ketanchaudhari1998@gmail.com>
2021-08-14 21:42:58 +0530

    basic connection with helloWorld lambda

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+ * `cdk bootstrap`   The first time you deploy an AWS CDK app into an environment (account/region), you can install a “bootstrap stack”. This stack includes resources that are used in the toolkit’s operation. For example, the stack includes an S3 bucket that is used to store templates and assets during the deployment process.
--- lib/emoji-pop-stack.ts
+++ lib/emoji-pop-stack.ts
@@ -1,9 +1,55 @@
 import * as cdk from '@aws-cdk/core';
+import * as apigw2 from '@aws-cdk/aws-apigatewayv2';
+import * as apigw2i from '@aws-cdk/aws-apigatewayv2-integrations';
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as iam from "@aws-cdk/aws-iam";
 
 export class EmojiPopStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const counterLambda = new lambda.Function(this, "counterLambda", {
+      handler: "emoji-rain-handler.handler2",
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        TEST: "test-env",
+      }
+    });
+
+    const webSocketApi = new apigw2.WebSocketApi(this, 'mywsapi', {
+      connectRouteOptions: { integration: new apigw2i.LambdaWebSocketIntegration({ handler: counterLambda }) },
+      disconnectRouteOptions: { integration: new apigw2i.LambdaWebSocketIntegration({ handler: counterLambda }) },
+      defaultRouteOptions: { integration: new apigw2i.LambdaWebSocketIntegration({ handler: counterLambda }) },
+    });
+    
+    webSocketApi.addRoute('sendEmoji', {
+      integration: new apigw2i.LambdaWebSocketIntegration({
+        handler: counterLambda,
+      }),
+    });
+    
+    const webSocketStage = new apigw2.WebSocketStage(this, 'mystage', {
+      webSocketApi,
+      stageName: 'production',
+      autoDeploy: true,
+    });
+    
+    const webSocketURL = webSocketStage.url;
+    // wss://${this.api.apiId}.execute-api.${s.region}.${s.urlSuffix}/${urlPath}
+    const callbackURL = webSocketStage.callbackUrl;
+    // https://${this.api.apiId}.execute-api.${s.region}.${s.urlSuffix}/${urlPath}
+
+    counterLambda.addEnvironment("CALLBACK_URL", callbackURL);
+    counterLambda.addEnvironment("WEB_SOCKET_URL", webSocketURL);
+    
+    // there must be a better way to do this!!.
+    counterLambda.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonAPIGatewayInvokeFullAccess"))
+    // addPermission to give resouce base policy-> pincliple is who will have the access.
+
+
+
+    
+    
   }
 }
