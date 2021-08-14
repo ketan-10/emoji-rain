3fb9a3637c1babd448d45eb712fc845f3bc95c81
wiwium <ketanchaudhari1998@gmail.com>
2021-08-14 18:34:27 +0530

    fixed npm dependencies

--- lib/emoji-pop-stack.ts
+++ lib/emoji-pop-stack.ts
@@ -4,6 +4,6 @@ export class EmojiPopStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    
   }
 }
