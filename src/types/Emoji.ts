257ab3e3afb2dd5f9d140afba63e75e4f4f200d9
wiwium <ketanchaudhari1998@gmail.com>
2021-08-15 23:50:20 +0530

    update Websocket url

--- src/types/Emoji.ts
+++ src/types/Emoji.ts
@@ -1,4 +1,4 @@
-
+import artifact from "../../artifact/cdk-outputs.json/cdk-outputs.json";
 export interface Emoji {
   value: string,
   id: string,
@@ -9,6 +9,6 @@ export const ANIMATION_TIME = 2000;
 export const BOTTOM_ANIMATION_LEN = 500;
 export const SKEW_MEAN = 60;
 export const SKEW_VARIANCE = 1000;
-
-export const URL = "wss://9hn172cu3a.execute-api.us-east-2.amazonaws.com/production";
+  
+export const URL = artifact.EmojiPopStack.WEBSOCKETURL;
 export const EMOJIES = "😀😁😂🤣😎🤗😫😯🤐😚😛😝😜😖😤😬😨😧😭😢🤪😴";
